3099f1cceec4fe24796f71ec3316deb662b013e0
Alexey <84139032+VaTTTT@users.noreply.github.com>
2023-07-23 18:22:46 +0300

    Iteration 4

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
-    [SerializeField] private float _changingSpeed;
     [SerializeField] private Player _owner;
+    [SerializeField] private float _changingSpeed;
+
 
     private void Start()
     {
@@ -16,13 +18,19 @@ public class HealthBar : MonoBehaviour
         _slider.value = _owner.CurrentHealth;
     }
 
-    public IEnumerator CorrectValue()
+    public void UpdateHealth()
+    {
+        StartCoroutine(ChangeValue());
+    }
+
+    private IEnumerator ChangeValue()
     {
         while (_owner.CurrentHealth != _slider.value)
         {
             _slider.value = Mathf.MoveTowards(_slider.value, _owner.CurrentHealth, _changingSpeed * Time.deltaTime);
-            
             yield return null;
         }
+        
+        StopCoroutine(ChangeValue());
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,25 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    public float CurrentHealth => _currentHealth;
+
+    [SerializeField] private UnityEvent _healthChanged = new UnityEvent();
     [SerializeField] private float _maxHealth;
     [SerializeField] private float _currentHealth;
-    [SerializeField] private HealthBar _healthBar;
-
-    //public float MaxHealth => _maxHealth;
-    public float CurrentHealth => _currentHealth;
+    
+    public event UnityAction HealthChanged
+    { 
+        add => _healthChanged.AddListener(value);
+        remove => _healthChanged.RemoveListener(value);
+    }
 
     public void TakeDamage(int health)
     {
         _currentHealth = Mathf.Clamp(_currentHealth - health, 0, _maxHealth);
-        StartCoroutine(_healthBar.CorrectValue());
+        _healthChanged.Invoke();
     }
 
     public void ReceiveHeal(int health)
     {
         _currentHealth = Mathf.Clamp(_currentHealth + health, 0, _maxHealth);
-        StartCoroutine(_healthBar.CorrectValue());
+        _healthChanged.Invoke();
     }
 }
